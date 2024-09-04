995b3bd4d0e790e816a7775aa2d4da75b864ca5b
badrievad <zzdl4@bk.ru>
2024-09-04 17:07:46 +0300

    [update] Fixed click hovers in the menu

--- app/static/js/leasing-calculator-sidebar.js
+++ app/static/js/leasing-calculator-sidebar.js
@@ -1,26 +1,3 @@
-function showTab(tabId) {
-    var tabs = document.getElementsByClassName('tab-content');
-    for (var i = 0; i < tabs.length; i++) {
-        tabs[i].classList.remove('active');
-    }
-    document.getElementById(tabId).classList.add('active');
-
-    var tabButtons = document.getElementsByClassName('tab');
-    for (var i = 0; i < tabButtons.length; i++) {
-        tabButtons[i].classList.remove('active');
-    }
-    event.target.classList.add('active');
-}
-
-function toggleSubmenu(id) {
-    var submenu = document.getElementById(id);
-    if (submenu.classList.contains('show')) {
-        submenu.classList.remove('show');
-    } else {
-        submenu.classList.add('show');
-    }
-}
-
 function showForm(formId) {
     document.querySelectorAll('.deal-content .card').forEach(card => {
         card.classList.add('hidden');
@@ -33,20 +10,3 @@ window.onload = function () {
     document.getElementById("main-content").classList.remove("shrink");
 
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('.clickable a').addEventListener('click', function (event) {
-        event.preventDefault(); // Предотвращаем стандартное действие ссылки
-
-        // Получаем цель прокрутки из атрибута href
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-
-        if (targetElement) {
-            window.scrollTo({
-                top: targetElement.offsetTop,
-                behavior: 'smooth'
-            });
-        }
-    });
-});

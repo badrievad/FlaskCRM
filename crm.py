f8ec56a1ff7b2754848e4a3ca31a688453280ae7
badrievad <zzdl4@bk.ru>
2024-12-09 19:31:16 +0300

    fixed

--- crm.py
+++ crm.py
@@ -5,4 +5,4 @@ from app.config import SOCKETIO_BACKEND
 app = create_app(debug=True)
 
 if __name__ == "__main__":
-    socketio.run(app, port=8005, host="0.0.0.0")
+    socketio.run(app, port=8005, host="0.0.0.0", message_queue=SOCKETIO_BACKEND)

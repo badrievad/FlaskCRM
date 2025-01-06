f6fda4a7ac0ec174fc81650da3ab974cbb1bcbb4
badrievad <zzdl4@bk.ru>
2025-01-06 16:54:01 +0300

    updated client app

--- app/client_application/routes.py
+++ app/client_application/routes.py
@@ -10,7 +10,7 @@ def latest_version():
     """
     return jsonify(
         {
-            "version": "1.0.0",  # Текущая версия на сервере
-            "download_url": "https://example.com/downloads/client_app_v1.1.0.exe",  # Ссылка на скачивание
+            "version": "1.0.1",  # Текущая версия на сервере
+            "download_url": "https://storage.yandexcloud.net/commercial-offers1/client-app/client_app_v1.1.0.exe?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEvYd17LdIreIRxjtTa3G4%2F20250106%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20250106T133136Z&X-Amz-Expires=2592000&X-Amz-Signature=9E1A2CCA4B041652570ABA7C162E1FBACA337332D2CBAB88EFED8ED768FF5D1D&X-Amz-SignedHeaders=host",  # Ссылка на скачивание
         }
     )
---
+++ app/static/js/risk_department/go-to-folder.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+	document.querySelectorAll('.icon-item').forEach(item => {
+		item.addEventListener('click', async () => {
+			const folderPath = item.getAttribute('data-folder-path')
+			if (folderPath) {
+				try {
+					const response = await fetch('http://localhost:5001/open-folder', {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+						body: JSON.stringify({ folder_path: folderPath }),
+					})
+
+					if (!response.ok) {
+						// Обработка HTTP ошибок
+						console.error(
+							`HTTP ошибка: ${response.status} ${response.statusText}`
+						)
+						return
+					}
+
+					const data = await response.json()
+					if (data.status === 'success') {
+						console.log('Папка успешно открыта.')
+					} else {
+						console.error('Ошибка на сервере:', data.message)
+					}
+				} catch (error) {
+					console.error('Ошибка запроса:', error.message)
+				}
+			} else {
+				console.error('Путь к папке не найден.')
+			}
+		})
+	})
+})

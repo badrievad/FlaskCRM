5f27f905348ab00fcbefe00c32ef774fac0275eb
badrievad <zzdl4@bk.ru>
2025-01-14 19:40:18 +0300

    Updated routes for client app

--- app/client_application/routes.py
+++ app/client_application/routes.py
@@ -1,7 +1,7 @@
 from flask import jsonify
 
 from . import client_application_bp
-
+from ..config import CLIENT_APPLICATION_VERSION, CLIENT_APPLICATION_URL
 
 @client_application_bp.route("/latest-version", methods=["GET"])
 def latest_version():
@@ -10,7 +10,7 @@ def latest_version():
     """
     return jsonify(
         {
-            "version": "1.0.2",  # Текущая версия на сервере
-            "download_url": "https://storage.yandexcloud.net/commercial-offers1/client-app/client_app_v1.0.2.exe?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJEvYd17LdIreIRxjtTa3G4%2F20250107%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20250107T092130Z&X-Amz-Expires=3600&X-Amz-Signature=6B9848F1241687D1E272FE769940C572DDFD10511EC8ED76D98439FB32BB9B02&X-Amz-SignedHeaders=host",  # Ссылка на скачивание
+            "version": CLIENT_APPLICATION_VERSION,  # Текущая версия на сервере
+            "download_url": CLIENT_APPLICATION_URL,  # Ссылка на скачивание
         }
     )
